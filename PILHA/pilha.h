9947bf04dd361705a9421d82fee3ed7aee12a414
Cilinaldo da Silva <cilinaldo@gmail.com>
2020-10-21 01:07:30 -0300

    estrutura de dados: pilha

---
+++ PILHA/pilha.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef char item;
+typedef struct {
+  item * v;
+  int topo;
+  int tam;
+} pilha;
+
+pilha * criaPilha (int tam);
+void destroiPilha (pilha * p);
+void empilha (pilha * p, item x);
+item desempilha (pilha * p);
+int pilhaVazia (pilha * p);
+item topoDaPilha (pilha *p);
+pilha * aumentaPilha (pilha * p); 
+
+
+
